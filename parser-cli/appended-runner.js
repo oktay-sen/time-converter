be0cf3d047f9feab33d9f7aad4a6ce96e633fddc
Oktay Sen <oktay@oktay.io>
2020-10-25 22:16:06 +0000

    Removed CLI

--- parser-cli/appended-runner.js
+++
@@ -1,7 +0,0 @@
-const util = require('util')
-
-if (process.argv.length < 3) {
-    console.error("Usage: npm run parser -- 'Time phrase'")
-} else {
-    console.log(util.inspect(generatedParser.parse(process.argv[2]), false, null, true))
-}
